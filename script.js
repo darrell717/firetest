0f0a687cd998c99160353999532207ee22cfdba1
darrell717 <bautistadarrell26@gmail.com>
2025-02-21 09:54:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function show() {
+    var uploadDiv = document.getElementById('uploadDiv');
+    uploadDiv.style.display = 'flex';
+}
+
+function hide() {
+    var uploadDiv = document.getElementById('uploadDiv');
+    uploadDiv.style.display = 'none';
+}
+
+function strandShow() {
+    var showStrand = document.getElementById('sections2container');
+    showStrand.style.display = 'flex';
+}
+
+function strandHide() {
+    var showStrand = document.getElementById('sections2container')
+    showStrand.style.display = 'none';
+}
